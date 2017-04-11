6c8c8309b504697e163c421e45d7619e06a9ce03
MrIlliteracy <mrilliteracy@hotmail.com>
2017-04-10 18:37:08 -0700

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
 call electron-packager . CemuManager --overwrite --asar --out=builds/ --platform=win32 --arch=ia32 --icon "./ico.ico"
 echo Application built
-echo Press any key to exit...
-set /p input=
+pause
