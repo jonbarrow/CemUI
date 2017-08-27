1b73fd22f42fecd523c2dfa5523f37e0050d68f3
RedDucks <jonbarrow1998@gmail.com>
2017-08-27 15:56:35 -0400

    Games now load properly from the games dir
    Added game loading from the games dir, added proper wud support with rpx support., better file organization and extra features being added. Currently the games folder path is hard coded, needs to be changed before release.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var electron = require('electron'),
-    fs = require('fs'),
-    originalFs = require('original-fs'),
+    fs = require('fs-extra'),
+	fs_o = require('original-fs'),
+	async = require('async'),
+	XMLParser = require('pixl-xml'),
+	low = require('lowdb'),
+	FileSync = require('lowdb/adapters/FileSync'),
+	game_storage,
     path = require('path'),
-    url = require('url'),
+	url = require('url'),
+	tga2png = require('tga2png'),
+    png2ico = require('png-to-ico'),
+    request = require('request'),
     dialog = electron.dialog,
     BrowserWindow = electron.BrowserWindow,
     ipcMain = electron.ipcMain,
     app = electron.app;
-
 
 let ApplicationWindow; // Main application window
 
@@ -28,9 +35,8 @@ function createWindow(file) { // Creates window from file
 	ApplicationWindow.setMenu(null);
 }
 
-
-
 app.on('ready', function() {
+	init();
     createWindow('index');
 	ApplicationWindow.webContents.on('new-window', function(event, url) {
   		event.preventDefault();
@@ -43,3 +49,192 @@ app.on('window-all-closed', () => {
     	app.quit(); // OSX shit
   	}
 })
+
+function init() {
+	fs.ensureDirSync(path.join(__dirname, '/cache/images'));
+	fs.ensureDirSync(path.join(__dirname, '/cache/json'));
+	if (!fs.existsSync(path.join(__dirname, '/cache/json/games.json'))) {
+		fs.createFileSync(path.join(__dirname, '/cache/json/games.json'));
+	}
+	game_storage = low(new FileSync(path.join(__dirname, '/cache/json/games.json')));
+	game_storage.defaults({games: []}).write();
+	loadGames('C:/Users/halol/Documents/Games/WiiU');
+}
+
+function loadGames(dir) {
+	fs.readdir(dir, (error, files) => {
+		if (error) throw error;
+		for (var i=0;i<files.length;i++) {
+			var test = game_storage.get('games').find({ path: dir + '/' + files[i] }).value(),
+				is_wud = false;
+			if (test) {
+				continue;
+			}
+
+			if (path.extname(dir + '/' + files[i]) == '.wud') {
+				is_wud = true;
+			}
+
+			if (isRPX(dir + '/' + files[i]) || is_wud) {
+				async.waterfall([
+					function(cb) {
+						try {
+							if (!is_wud) {
+								var xml = XMLParser.parse(dir + '/' + files[i] + '/meta/meta.xml');
+							}
+						} catch (error) {
+							return cb(true);
+						}
+						
+						
+						if (!is_wud) {
+							if (!xml || !xml.title_id) {
+								return cb(true);
+							}
+						}
+
+						cb(null, is_wud);
+					},
+					function(is_wud, cb) {
+						var name = files[i];
+						getGameData(dir + '/' + files[i], is_wud, (error, data) => {
+							if (error) {
+								return cb(true);
+							}
+
+							cb(null, data, name, is_wud);
+						});
+					},
+					function(data, name, is_wud, cb) {
+						if (data.game_background_url && data.game_background_url !== '') {
+							fs.ensureDirSync(path.join(__dirname, '/cache/images/' + data.game_title_id + '/screenshots'));
+							request(data.game_background_url)
+								.on('error', () => {
+									return cb(true);
+								})
+								.pipe(fs.createWriteStream(path.join(__dirname, '/cache/images/' + data.game_title_id + '/screenshots/1.jpg')))
+								.on('error', () => {
+									return cb(true);
+								})
+								.on('close', () => {
+									cb(null, data, name, is_wud)
+								});
+						} else {
+							cb(null, data, name, is_wud);
+						}
+						
+					},
+					function(data, name, is_wud, cb) {
+						if (data.game_boxart_url && data.game_boxart_url !== '') {
+							request(data.game_boxart_url)
+								.on('error', () => {
+									return cb(true);
+								})
+								.pipe(fs.createWriteStream(path.join(__dirname, '/cache/images/' + data.game_title_id + '/box.jpg')))
+								.on('error', () => {
+									return cb(true);
+								})
+								.on('close', () => {
+									cb(null, data, name, is_wud)
+								});
+						} else {
+							fs.createReadStream('./defaults/box.jpg')
+								.pipe(fs.createWriteStream(path.join(__dirname, '/cache/images/' + data.game_title_id + '/box.jpg')));
+							
+							cb(null, data, name, is_wud);
+						}
+						
+					},
+					function(data, name, is_wud, cb) {
+						if (!is_wud) {
+							tga2png(dir + '/' + name + '/meta/iconTex.tga').then(buffer=> {
+								png2ico(buffer).then((buffer) => {
+									fs.writeFileSync(path.join(__dirname, '/cache/images/' + data.game_title_id + '/icon.ico'), buffer);
+									cb(null, data, name, is_wud);
+								}).catch(() => {
+									return cb(true);
+								});
+							}, (error) => {
+								return cb(true);
+							});
+						} else {
+							fs.createReadStream('./defaults/icon.ico')
+								.pipe(fs.createWriteStream(path.join(__dirname, '/cache/images/' + data.game_title_id + '/icon.ico')));
+							
+							cb(null, data, name, is_wud);
+						}
+					}
+				], function(error, data, name, is_wud) {
+					if (error) return;
+
+					var game_data = {
+						is_wud: is_wud,
+						title_id: data.game_title_id,
+						product_code: data.game_product_code,
+						path: dir + '/' + name,
+						name: data.game_title,
+						name_clean: data.game_title_clean,
+						release_date: data.game_release_date,
+						publisher: data.game_publisher,
+						developer: data.game_developer,
+						region: data.game_region,
+						release_region: data.game_release_region,
+						rating: data.game_esrb,
+						max_players: data.game_max_player,
+						co_op: data.game_coop,
+						description: data.game_overview
+					}
+					game_storage.get('games').push(game_data).write();
+				});
+			}
+		}
+	});
+}
+
+function isRPX(game_path) {
+	var stats = fs.lstatSync(game_path);
+	if (!stats) return false;
+
+	if (stats.isDirectory()) {
+		var subfolders = fs.readdirSync(game_path);
+		if (subfolders.contains('code') && subfolders.contains('content') && subfolders.contains('meta')) {
+			if (fs.pathExistsSync(game_path + '/meta/meta.xml')) {
+				var rom = fs.readdirSync(game_path + '/code').filter(/./.test, /\.rpx$/i);
+				if (!rom || rom.length < 0) return false;
+			} else return false;
+		} else return false;
+	} else return false;
+
+	return true;
+}
+
+function getGameData(game_path, is_wud, callback) {
+	var game_data = {},
+		post_code, post_type, xml;
+	if (is_wud) {
+		post_type = 'product_code';
+		post_code = getProductCode(game_path);
+		console.log(post_code)
+	} else {
+		xml = XMLParser.parse(game_path + '/meta/meta.xml');
+		post_type = 'title_id';
+		post_code = [xml.title_id._Data.slice(0, 8), '-', xml.title_id._Data.slice(8)].join('');
+	}
+
+	request('http://104.236.44.105/api/GetGame/?' + post_type + '=' + post_code, (error, response, body) => {
+		body = JSON.parse(body);
+		if (!body || body.error) return callback(true);
+		return callback(null, body)
+	})
+}
+
+function getProductCode(file, cb) {
+	var fd = fs.openSync(file, 'r'),
+		buffer = new Buffer(10),
+		productCode = fs.readSync(fd, buffer, 0, 10, 0);
+	return buffer.toString('utf8', 0, productCode);
+}
+
+Array.prototype.contains = function(el) {
+    return this.indexOf(el) > -1;
+}
--- test.js
+++ test.js
@@ -11,14 +11,16 @@ request('http://thegamesdb.net/banners/screenshots/', (error, status, body) => {
     console.log(status.statusCode);
 })*/
 
-var request = require('request'),
+var request = require('request').defaults({headers: {'User-Agent': "Allow My Robot 188167AE4E7920FD", 'Connection': 'keep-alive'}}),
+    http = require('http-get'),
     cheerio = require('cheerio'),
+    XMLParser = require('pixl-xml'),
     fs = require('fs');
 
 const SITE_MAP_URL = 'http://sitemap.gamefaqs.com';
 const SITE_URL = 'http://gamefaqs.com';
 
-request(SITE_MAP_URL + '/images/wii-u/', (error, status, body) => {
+/*request(SITE_MAP_URL + '/images/wii-u/', (error, status, body) => {
     if (status.statusCode === 200) {
         var $ = cheerio.load(body),
             elements = $('body').find('ul').find('li');
@@ -33,7 +35,9 @@ request(SITE_MAP_URL + '/images/wii-u/', (error, status, body) => {
                                 var $ = cheerio.load(body),
                                     images = $('body').find('table.contrib').find('img.img100.imgboxart');
                                 for (var k=images.length-1;k>=0;k--) {
-                                    console.log(images[k].parent.name)
+                                    if (images[k].parent.name == 'div') {
+                                        downloadicon(images[k].attribs.src);
+                                    }
                                 }
                             }
                         })
@@ -43,3 +47,64 @@ request(SITE_MAP_URL + '/images/wii-u/', (error, status, body) => {
         }
     }
 });
+
+function downloadicon(url) {
+    request(url).pipe(fs.createWriteStream('./icons/' + url.replace(/\//g, '').replace(/\\/g, '').replace(/:/g, '')).on('error', downloadicon(url)))
+    .on('close', () => {
+        console.log(url)
+    })
+    .on('error', () => {
+        downloadicon(url);
+    });
+}*/
+
+/*var links = fs.readFileSync('games.txt').toString().split('\r\n')
+for (var i=links.length-1;i>=0;i--) {
+    downloadicons(links[i])
+}
+
+function downloadicons(url) {
+    if (!url) return;
+    if (url.length < 10) return;
+    setTimeout(function() {
+        request(url, (error, status, body) => {
+            if (status && status.statusCode === 200) {
+                var $ = cheerio.load(body),
+                    images = $('body').find('table.contrib').find('img.img100.imgboxart');
+                for (var i=images.length-1;i>=0;i--) {
+                    if (images[i].parent.name == 'div') {
+                        request
+                        .get(images[i].attribs.src)
+                        .on('error', (error) => {
+                            console.log('2')
+                            downloadicons(url);
+                        })
+                        .pipe(fs.createWriteStream('./icons/' + images[i].attribs.src.replace(/\//g, '').replace(/\\/g, '').replace(/:/g, '')))
+                    }
+                }
+            } else if (error) {
+                console.log('1')
+                downloadicons(url);
+            }
+        })
+    }.bind(this), 1000);
+}*/
+
+/*var xml = XMLParser.parse('w.xml'),
+    dict = {};
+
+xml.companies.company.forEach(function(comp) {
+    dict[comp.name] = comp.code;
+}, this);
+
+console.log(dict)*/
+
+var fs = require('fs'),
+    tga2png = require('tga2png'),
+    png2ico = require('png-to-ico');
+
+tga2png('C:/Users/halol/Documents/Games/WiiU/Super Mario Maker [AMAE0101] - Copy/meta/iconTex.tga').then(buffer=> {
+    png2ico(buffer).then((buffer) => {
+        fs.writeFileSync('smm-ico.ico', buffer);
+    }).catch(console.log);
+}, error => console.log);
