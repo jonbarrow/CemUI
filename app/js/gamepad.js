f24558317498388a968731ff09eefcdf83452044
RedDucks <jonbarrow1998@gmail.com>
2017-09-02 01:49:07 -0400

    Sound effects
    Controller sound effects

--- app/js/gamepad.js
+++ app/js/gamepad.js
@@ -1,3 +1,7 @@
+var sound_switch = new Audio('sounds/pop_drip.wav'),
+    sound_switch_error = new Audio('sounds/digi_chirp.wav');
+
+
 Controller.search({
     settings: {
         useAnalogAsDpad: "both"
@@ -17,27 +21,48 @@ window.addEventListener('gc.button.press', function(event) {
         case 'DPAD_RIGHT':
         case 'RIGHT_SHOULDER':
         case 'RIGHT_SHOULDER_BOTTOM':
+            sound_switch.pause();
+            sound_switch.currentTime = 0.0;
+
             var current_selected = document.getElementsByClassName('controller-active'),
                 carry = current_selected[0];
-            if (!carry.nextElementSibling) return;
+                if (!carry.nextElementSibling) {
+                    sound_switch_error.pause();
+                    sound_switch_error.currentTime = 0.0;
+                    sound_switch_error.play();
+                    return;
+                }
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
             }
             carry.nextElementSibling.classList.add('controller-active');
+            sound_switch.play();
             break;
 
         case 'DPAD_LEFT':
         case 'LEFT_SHOULDER':
         case 'LEFT_SHOULDER_BOTTOM':
+            sound_switch.pause();
+            sound_switch.currentTime = 0.0;
+
             var current_selected = document.getElementsByClassName('controller-active'),
                 carry = current_selected[0];
-            if (!carry.previousElementSibling) return;
+            if (!carry.previousElementSibling) {
+                sound_switch_error.pause();
+                sound_switch_error.currentTime = 0.0;
+                sound_switch_error.play();
+                return;
+            }
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
             }
             carry.previousElementSibling.classList.add('controller-active');
+            sound_switch.play();
             break;
         case 'DPAD_DOWN':
+            sound_switch.pause();
+            sound_switch.currentTime = 0.0;
+
             // NEEDS WORK
             var current_selected = document.getElementsByClassName('controller-active'),
                 carry = current_selected[0];
@@ -45,11 +70,21 @@ window.addEventListener('gc.button.press', function(event) {
             var pos = carry.getBoundingClientRect(),
                 next = document.elementFromPoint(pos.left + 20, carry.scrollTop + pos.top + carry.clientHeight + 20);
 
-            if (!next) return;
+            if (!next) {
+                sound_switch_error.pause();
+                sound_switch_error.currentTime = 0.0;
+                sound_switch_error.play();
+                return;
+            }
             if (next.tagName == 'IMG') {
                 next = next.parentElement;
             }
-            if (!next.classList.contains('grid-item')) return;
+            if (!next.classList.contains('grid-item')) {
+                sound_switch_error.pause();
+                sound_switch_error.currentTime = 0.0;
+                sound_switch_error.play();
+                return;
+            }
 
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
@@ -67,8 +102,12 @@ window.addEventListener('gc.button.press', function(event) {
             
             
             next.classList.add('controller-active');
+            sound_switch.play();
             break;
         case 'DPAD_UP':
+            sound_switch.pause();
+            sound_switch.currentTime = 0.0;
+
             // NEEDS WORK
             var current_selected = document.getElementsByClassName('controller-active'),
                 carry = current_selected[0];
@@ -76,11 +115,21 @@ window.addEventListener('gc.button.press', function(event) {
             var pos = carry.getBoundingClientRect();
                 next = document.elementFromPoint(pos.left + 20, carry.scrollTop + pos.top - carry.clientHeight + 20);
             
-            if (!next) return;
+            if (!next) {
+                sound_switch_error.pause();
+                sound_switch_error.currentTime = 0.0;
+                sound_switch_error.play();
+                return;
+            }
             if (next.tagName == 'IMG') {
                 next = next.parentElement;
             }
-            if (!next.classList.contains('grid-item')) return;
+            if (!next.classList.contains('grid-item')) {
+                sound_switch_error.pause();
+                sound_switch_error.currentTime = 0.0;
+                sound_switch_error.play();
+                return;
+            }
 
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
@@ -96,6 +145,8 @@ window.addEventListener('gc.button.press', function(event) {
             document.body.appendChild(tst);*/
             
             next.classList.add('controller-active');
+            sound_switch.play();
+            break;
         default:
             break;
     }
