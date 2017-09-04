22db7474031d22fb17e85f0ead024caceeef066e
mrjvs <jellevs@gmail.com>
2017-09-04 22:06:17 +0200

    UI added most played and suggested to UI and made modals for suggested games.

--- app/js/main.js
+++ app/js/main.js
@@ -59,7 +59,7 @@ ipcRenderer.on('game_folder_loading', function(event, data) {
 
 ipcRenderer.on('init_complete', function(event, data) {
     var games = data.library;
-    for (var i=0;i<games.length;i++) {
+    for (var i=0,length = games.length ;i<length;i++) {
         var game = games[i],
             wrapper = document.createElement('div'),
             fav = document.createElement('i'),
@@ -92,6 +92,10 @@ ipcRenderer.on('init_complete', function(event, data) {
         
         games_lib.appendChild(wrapper);
     }
+    
+    addToGrid(data.suggested,'suggest_grid');
+    addToGrid(data.most_played,'most_grid');
+    
     var count = games.length;
     var high = document.getElementsByClassName('highlight')[0];
         if (typeof high != 'undefined') {
@@ -111,7 +115,35 @@ addEvent(window, 'load', function() {
     ipcRenderer.send('init');
 });
 
-function createModal(game) {
+function addToGrid(arr,id) {
+    console.log(arr);
+    var children = document.getElementById(id).children;
+    for (var i=0,length = children.length ;i<length;i++) {
+        if (typeof arr[i] != 'undefined') {
+
+            var wrapper = children[i],
+                box = document.createElement('div'),
+                game = arr[i];
+
+            if (typeof game.game_boxart_url == 'undefined') {
+                box.style.backgroundImage = 'url("../cache/images/' + game.title_id + '/box.jpg")';
+                wrapper.setAttribute('data-modal-id', game.title_id);
+            } else {
+                box.style.backgroundImage = 'url("' + game.game_boxart_url + '")';
+                wrapper.setAttribute('data-modal-id', game.game_title_id);
+                createModal(game,true);
+            }
+            box.classList = 'boxart';
+            box.onclick = function() {
+                openModal(this.parentElement.getAttribute('data-modal-id'));
+            }
+
+            wrapper.appendChild(box);
+        }
+    }
+}
+
+function createModal(game,isSuggest) {
     var modal = document.createElement('div'),
         modal_content = document.createElement('div'),
         close = document.createElement('span'),
@@ -120,17 +152,55 @@ function createModal(game) {
         box = document.createElement('img'),
         title = document.createElement('div'),
         buttons = document.createElement('div'),
-        play_button = document.createElement('p'),
-        folder_button = document.createElement('p'),
-        shortcut_button = document.createElement('p'),
-        game_settings_button = document.createElement('p'),
         desc = document.createElement('div'),
         sst = document.createElement('div'),
         screenshots_list = document.createElement('div');
 
 
-
-    modal.id = game.title_id;
+    if (isSuggest) {
+        var visit_button = document.createElement('p');
+        title.innerHTML = '<h2 class="txt-s-32 txt-c-black">' + game.game_title_clean + '</h2>';
+        box.src = game.game_boxart_url;
+        visit_button.classList = 'txt-s-16 txt-bold button button-small';
+        visit_button.innerHTML = 'Visit game website';
+        visit_button.onclick = function() {
+            alert('coming soon');
+        }
+        desc.innerHTML = '<p class="txt-s-16 txt-c-gray">' + game.game_overview + '</p>';
+        desc.innerHTML += '<br><h2 class="txt-s-16 txt-c-black">Playability</h2>';
+        desc.innerHTML += '<p class="txt-s-16 txt-c-gray">' + game.game_playability + '</p>';
+        modal.id = game.game_title_id;
+    } else {
+        var play_button = document.createElement('p'),
+            folder_button = document.createElement('p'),
+            shortcut_button = document.createElement('p'),
+            game_settings_button = document.createElement('p');
+        
+        title.innerHTML = '<h2 class="txt-s-32 txt-c-black">' + game.name + '</h2>';
+        play_button.classList = 'txt-s-16 txt-bold button button-small play-button';
+        play_button.innerHTML = 'Play';
+        play_button.onclick = function() {
+            ipcRenderer.send('play_rom', this.parentElement.parentElement.parentElement.parentElement.id);
+        }
+        folder_button.classList = 'txt-s-16 txt-bold button button-small folder-button';
+        folder_button.innerHTML = 'Show in folder';
+        folder_button.onclick = function() {
+            ipcRenderer.send('show_rom', this.parentElement.parentElement.parentElement.parentElement.id);
+        }
+        shortcut_button.classList = 'txt-s-16 txt-bold button button-small shortcut-button';
+        shortcut_button.innerHTML = 'Create desktop shortcut';
+        shortcut_button.onclick = function() {
+            ipcRenderer.send('make_shortcut', this.parentElement.parentElement.parentElement.parentElement.id);
+        }
+        game_settings_button.classList = 'txt-s-16 txt-bold button button-small game-settings-button';
+        game_settings_button.innerHTML = 'Settings';
+        game_settings_button.onclick = function() {
+            alert('Coming soon');
+        }
+        box.src = '../cache/images/' + game.title_id + '/box.jpg';
+        desc.innerHTML = '<p class="txt-s-16 txt-c-gray">' + game.description + '</p>';
+        modal.id = game.title_id;
+    }
     modal.classList = 'modal';
     modal.onclick = function(event) {
         closeModal();
@@ -146,41 +216,23 @@ function createModal(game) {
     close.innerHTML = '&times;';
     modal_grid_game.classList = 'modal-grid-game';
     art.classList = 'art';
-    box.src = '../cache/images/' + game.title_id + '/box.jpg';
     title.classList = 'title';
-    title.innerHTML = '<h2 class="txt-s-32 txt-c-black">' + game.name + '</h2>';
     buttons.classList = 'buttons';
-    play_button.classList = 'txt-s-16 txt-bold button button-small play-button';
-    play_button.innerHTML = 'Play';
-    play_button.onclick = function() {
-        ipcRenderer.send('play_rom', this.parentElement.parentElement.parentElement.parentElement.id);
-    }
-    folder_button.classList = 'txt-s-16 txt-bold button button-small folder-button';
-    folder_button.innerHTML = 'Show in folder';
-    folder_button.onclick = function() {
-        ipcRenderer.send('show_rom', this.parentElement.parentElement.parentElement.parentElement.id);
-    }
-    shortcut_button.classList = 'txt-s-16 txt-bold button button-small shortcut-button';
-    shortcut_button.innerHTML = 'Create desktop shortcut';
-    shortcut_button.onclick = function() {
-        ipcRenderer.send('make_shortcut', this.parentElement.parentElement.parentElement.parentElement.id);
-    }
-    game_settings_button.classList = 'txt-s-16 txt-bold button button-small game-settings-button';
-    game_settings_button.innerHTML = 'Settings';
-    game_settings_button.onclick = function() {
-        alert('Coming soon');
-    }
     desc.classList = 'desc';
-    desc.innerHTML = '<p class="txt-s-16 txt-c-gray">' + game.description + '</p>';
     sst.classList = 'ssT';
     sst.innerHTML = '<h2 class="txt-s-24 txt-c-black">Screenshots</h2>';
     screenshots_list.classList = 'ss';
-
+    
+    
+    if (isSuggest) {
+        buttons.appendChild(visit_button);
+    } else {
+        buttons.appendChild(play_button);
+        buttons.appendChild(folder_button);
+        buttons.appendChild(shortcut_button);
+        buttons.appendChild(game_settings_button);
+    }
     art.appendChild(box);
-    buttons.appendChild(play_button);
-    buttons.appendChild(folder_button);
-    buttons.appendChild(shortcut_button);
-    buttons.appendChild(game_settings_button);
     modal_grid_game.appendChild(art);
     modal_grid_game.appendChild(title);
     modal_grid_game.appendChild(buttons);
