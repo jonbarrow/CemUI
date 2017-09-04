062b7e679168517034f693d5c5986f9fc25c41ec
RedDucks <jonbarrow1998@gmail.com>
2017-09-03 22:56:55 -0400

    UI Better controller support
    Controller support should now be finalized. All intended functionality is present.

--- app/js/gamepad.js
+++ app/js/gamepad.js
@@ -9,22 +9,45 @@ Controller.search({
 });
 
 window.addEventListener('gc.controller.found', function(event) {
-    var games = document.getElementById('games-grid').getElementsByClassName('grid-item');
+    var games = document.getElementById('games-grid').getElementsByClassName('grid-item'),
+        highlighted = document.getElementsByClassName('highlight');
+
     if (!games[0]) return;
 
-    games[0].classList.add('controller-active');
-
+    if (highlighted) {
+        highlighted[0].classList.add('controller-active');
+    } else {
+        games[0].classList.add('controller-active');
+    }
+    
 }, false);
 
 window.addEventListener('gc.button.press', function(event) {
     console.log(event.detail.name)
 	switch (event.detail.name) {
         case 'FACE_1':
-            var current_selected = document.getElementsByClassName('controller-active')[0];
+            var current_selected = document.getElementsByClassName('controller-active grid-item')[0];
             if (!current_selected) return;
+            if (modal_open) {
+                var modal = document.getElementsByClassName('selected-modal')[0],
+                    buttons_list = modal.getElementsByClassName('buttons')[0],
+                    current_selected = buttons_list.getElementsByClassName('controller-active')[0];
+
+                if (current_selected) {
+                    current_selected.click();
+                }
+                
+                return;
+            }
             openModal(current_selected.getAttribute('data-modal-id'));
             break;
         case 'FACE_2':
+            var selected_buttons = document.getElementsByClassName('button controller-active');
+            if (selected_buttons.length > 0) {
+                for (var i=0;i<selected_buttons.length;i++) {
+                    selected_buttons[i].classList.remove('controller-active');
+                }
+            }
             closeModal();
             break;
         case 'DPAD_RIGHT':
@@ -32,19 +55,62 @@ window.addEventListener('gc.button.press', function(event) {
         case 'RIGHT_SHOULDER_BOTTOM':
             sound_switch.pause();
             sound_switch.currentTime = 0.0;
+            sound_switch_error.pause();
+            sound_switch_error.currentTime = 0.0;
 
-            var current_selected = document.getElementsByClassName('controller-active'),
-                carry = current_selected[0];
-                if (!carry.nextElementSibling) {
-                    sound_switch_error.pause();
-                    sound_switch_error.currentTime = 0.0;
-                    sound_switch_error.play();
-                    return;
+            if (modal_open) {
+                var modal = document.getElementsByClassName('selected-modal')[0],
+                    buttons_list = modal.getElementsByClassName('buttons')[0],
+                    current_selected = buttons_list.getElementsByClassName('controller-active'),
+                    carry = current_selected[0];
+                    
+                if (!carry) {
+                    buttons_list.firstElementChild.classList.add('controller-active');
+                } else if (carry.nextElementSibling) {
+                    for (var i=current_selected.length-1;i>=0;i--) {
+                        current_selected[i].classList.remove('controller-active');
+                    }
+                    carry.nextElementSibling.classList.add('controller-active');
                 }
+                return;
+            }
+
+            var current_selected = document.getElementsByClassName('controller-active grid-item'),
+                carry = current_selected[0];
+            
+            var pos = carry.getBoundingClientRect(),
+                next = document.elementFromPoint(pos.left + carry.clientWidth + 20, carry.scrollTop + pos.top + 20);
+
+
+            /* // Testing positons
+            var tst = document.createElement('div');
+            tst.style.position = 'absolute';
+            tst.style.backgroundColor = 'red';
+            tst.style.width = tst.style.height = carry.clientWidth/2 + 20 + 'px';
+            tst.style.top = pos.top + 20 + 'px';
+            tst.style.left = pos.left + carry.clientWidth + 20 + 'px';
+            document.body.appendChild(tst);*/
+            
+
+
+
+            if (!next) {
+                sound_switch_error.play();
+                return;
+            }
+            console.log(next.classList)
+            if (next.classList == 'boxart') {
+                next = next.parentElement;
+            }
+            if (!next.classList.contains('grid-item')) {
+                sound_switch_error.play();
+                return;
+            }
+
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
             }
-            carry.nextElementSibling.classList.add('controller-active');
+            next.classList.add('controller-active');
             sound_switch.play();
             break;
 
@@ -53,27 +119,71 @@ window.addEventListener('gc.button.press', function(event) {
         case 'LEFT_SHOULDER_BOTTOM':
             sound_switch.pause();
             sound_switch.currentTime = 0.0;
+            sound_switch_error.pause();
+            sound_switch_error.currentTime = 0.0;
 
-            var current_selected = document.getElementsByClassName('controller-active'),
+            if (modal_open) {
+                var modal = document.getElementsByClassName('selected-modal')[0],
+                    buttons_list = modal.getElementsByClassName('buttons')[0],
+                    current_selected = buttons_list.getElementsByClassName('controller-active'),
+                    carry = current_selected[0];
+                    
+                if (!carry) {
+                    buttons_list.firstElementChild.classList.add('controller-active');
+                } else if (carry.previousElementSibling) {
+                    for (var i=current_selected.length-1;i>=0;i--) {
+                        current_selected[i].classList.remove('controller-active');
+                    }
+                    carry.previousElementSibling.classList.add('controller-active');
+                }
+                return;
+            }
+
+            var current_selected = document.getElementsByClassName('controller-active grid-item'),
                 carry = current_selected[0];
-            if (!carry.previousElementSibling) {
-                sound_switch_error.pause();
-                sound_switch_error.currentTime = 0.0;
+            
+            var pos = carry.getBoundingClientRect(),
+                next = document.elementFromPoint(pos.left - carry.clientWidth/2, carry.scrollTop + pos.top + 20);
+
+
+            // Testing positons
+            /*var tst = document.createElement('div');
+            tst.style.position = 'absolute';
+            tst.style.backgroundColor = 'red';
+            tst.style.width = tst.style.height = carry.clientWidth/2 + 20 + 'px';
+            tst.style.top = pos.top + 20 + 'px';
+            tst.style.left = pos.left - carry.clientWidth/2 + 'px';
+            document.body.appendChild(tst);*/
+
+
+
+            if (!next) {
                 sound_switch_error.play();
                 return;
             }
+            if (next.classList == 'boxart') {
+                next = next.parentElement;
+            }
+            if (!next.classList.contains('grid-item')) {
+                sound_switch_error.play();
+                return;
+            }
+
             for (var i=current_selected.length-1;i>=0;i--) {
                 current_selected[i].classList.remove('controller-active');
             }
-            carry.previousElementSibling.classList.add('controller-active');
+            next.classList.add('controller-active');
             sound_switch.play();
             break;
         case 'DPAD_DOWN':
+            if (modal_open) return;
             sound_switch.pause();
             sound_switch.currentTime = 0.0;
+            sound_switch_error.pause();
+            sound_switch_error.currentTime = 0.0;
 
             // NEEDS WORK
-            var current_selected = document.getElementsByClassName('controller-active'),
+            var current_selected = document.getElementsByClassName('controller-active grid-item'),
                 carry = current_selected[0];
             
             var pos = carry.getBoundingClientRect(),
@@ -92,8 +202,6 @@ window.addEventListener('gc.button.press', function(event) {
 
 
             if (!next) {
-                sound_switch_error.pause();
-                sound_switch_error.currentTime = 0.0;
                 sound_switch_error.play();
                 return;
             }
@@ -101,8 +209,6 @@ window.addEventListener('gc.button.press', function(event) {
                 next = next.parentElement;
             }
             if (!next.classList.contains('grid-item')) {
-                sound_switch_error.pause();
-                sound_switch_error.currentTime = 0.0;
                 sound_switch_error.play();
                 return;
             }
@@ -115,11 +221,14 @@ window.addEventListener('gc.button.press', function(event) {
             sound_switch.play();
             break;
         case 'DPAD_UP':
+            if (modal_open) return;
             sound_switch.pause();
             sound_switch.currentTime = 0.0;
+            sound_switch_error.pause();
+            sound_switch_error.currentTime = 0.0;
 
             // NEEDS WORK
-            var current_selected = document.getElementsByClassName('controller-active'),
+            var current_selected = document.getElementsByClassName('controller-active grid-item'),
                 carry = current_selected[0];
     
             var pos = carry.getBoundingClientRect();
@@ -138,8 +247,6 @@ window.addEventListener('gc.button.press', function(event) {
             
 
             if (!next) {
-                sound_switch_error.pause();
-                sound_switch_error.currentTime = 0.0;
                 sound_switch_error.play();
                 return;
             }
@@ -147,8 +254,6 @@ window.addEventListener('gc.button.press', function(event) {
                 next = next.parentElement;
             }
             if (!next.classList.contains('grid-item')) {
-                sound_switch_error.pause();
-                sound_switch_error.currentTime = 0.0;
                 sound_switch_error.play();
                 return;
             }
