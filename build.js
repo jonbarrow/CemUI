f41dba36b7e1beedc5aba925d69b55fe0651bbe5
RedDucks <jonbarrow1998@gmail.com>
2017-09-09 02:33:15 -0400

    Backend cleaned up build.js

--- build.js
+++ build.js
@@ -19,36 +19,3 @@ builder.build({
 }).catch((error) => {
     throw error;
 });
-
-/*
-var packager = require('electron-packager');
-
-packager({
-	"dir": ".",
-	"name": "CemUI",
-	"overwrite": true,
-	"asar": true,
-	"out": "builds/",
-	"platform": "win32",
-	"arch": "ia32",
-    "icon": "./ico.ico",
-    "ignore": [
-        "node_modules",
-        "cache",
-        "builds"
-    ],
-	"win32metadata": {
-		"CompanyName": "CemUI",
-		"FileDescription": "A small launcher for the Cemu WiiU emulator made with Electron",
-		"OriginalFilename": "CemUI",
-		"ProductName": "Cemu Manager",
-		"InternalName": "CemUI"
-	}
-}, function (err, appPaths) {
-	if(err) {
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(appPaths);
-});
-*/
