f0c79df41da4378babfabe87e4f3a84dbb4a3534
RedDucks <jonbarrow1998@gmail.com>
2017-09-08 23:38:35 -0400

    Backend started gamepad button maps

--- app/js/gamepad.js
+++ app/js/gamepad.js
@@ -1,3 +1,61 @@
+const CEMU_BUTTON_NAMES = {
+    gamepad: {
+        A: 1,
+        B: 2,
+        X: 3,
+        Y: 4,
+        L: 5,
+        R: 6,
+        ZL: 7,
+        RL: 8,
+        "+": 9,
+        "-": 10,
+        "dpad-up": 11,
+        "dpad-down": 12,
+        "dpad-left": 13,
+        "dpad-right": 14,
+        "l-click": 15,
+        "l-up": 17,
+        "l-down": 18,
+        "l-left": 19,
+        "l-right": 20,
+        "r-click": 16,
+        "r-up": 21,
+        "r-down": 22,
+        "r-left": 23,
+        "r-right": 24,
+        blowmic: 25,
+        showscreen: 26
+    }
+}
+
+const CEMU_BUTTON_MAPPINGS = {
+    FACE_1: 'button_1',
+    FACE_2: 'button_2',
+    FACE_3: 'button_4',
+    FACE_4: 'button_8',
+    LEFT_SHOULDER: 'button_10',
+    RIGHT_SHOULDER: 'button_20',
+    LEFT_SHOULDER_BOTTOM: 'button_100000000',
+    RIGHT_SHOULDER_BOTTOM: 'button_4000000000',
+    START: 'button_80',
+    SELECT: 'button_40',
+    LEFT_ANALOG_BUTTON: 'button_100',
+    LEFT_ANALOG_STICK_UP: 'button_2000000000',
+    LEFT_ANALOG_STICK_DOWN: 'button_80000000',
+    LEFT_ANALOG_STICK_LEFT: 'button_1000000000',
+    LEFT_ANALOG_STICK_RIGHT: 'button_40000000',
+    RIGHT_ANALOG_BUTTON: 'button_200',
+    RIGHT_ANALOG_STICK_UP: 'button_10000000000',
+    RIGHT_ANALOG_STICK_DOWN: 'button_400000000',
+    RIGHT_ANALOG_STICK_LEFT: 'button_8000000000',
+    RIGHT_ANALOG_STICK_RIGHT: 'button_200000000',
+    DPAD_UP: 'button_4000000',
+    DPAD_DOWN: 'button_8000000',
+    DPAD_LEFT: 'button_10000000',
+    DPAD_RIGHT: 'button_20000000'
+}
+
 var sound_switch = new Audio('sounds/pop_drip.wav'),
     sound_switch_error = new Audio('sounds/digi_chirp.wav');
 
@@ -21,6 +79,10 @@ window.addEventListener('gc.controller.found', function(event) {
     }
     
 }, false);
+
+window.addEventListener('gc.analog.start', function(event) {
+    console.log(event.detail)
+})
 
 window.addEventListener('gc.button.press', function(event) {
     console.log(event.detail.name)
